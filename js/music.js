c325c45f350bf82f32ef6ab18f351f2bd61653fa
john lee <78394440+time-container@users.noreply.github.com>
2021-03-01 23:32:12 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,5 +1,5 @@
 function music_on(){
-    var musicbox=["https://blog.bestzuo.cn/blog-bgm.mp3","https://blog.bestzuo.cn/所爱隔山海.mp3","https://blog.bestzuo.cn/between worlds.mp3","https://blog.bestzuo.cn/stay with me.mp3", "https://s101.lzjoy.com/res/statics/fileupload/normal/202004/1c7b7bce5e8c52e912061.mp3?451b467b752b6af0d8c1badda9756f60f1e58917"];
+    var musicbox=["https://static-music-1253741713.cos.ap-beijing.myqcloud.com/blog-bgm.mp3","https://static-music-1253741713.cos.ap-beijing.myqcloud.com/dreamitpossible.mp3","https://static-music-1253741713.cos.ap-beijing.myqcloud.com/betweenworlds.mp3"];
     var audio1=document.getElementById('bg_music');
     if(audio1.paused){
         var index=Math.floor((Math.random()*musicbox.length));
